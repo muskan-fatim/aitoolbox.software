267b7b0ca43543a98d93a32b41732ab795aa0562
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2025-08-08 17:06:36 +0530

    Ads script

---
+++ src/components/NativeBannerAd.tsx
@@ -0,0 +1,19 @@
+
+"use client";
+
+import Script from "next/script";
+
+export default function NativeBannerAd() {
+  return (
+    <>
+      <Script
+        id="native-banner-ad"
+        strategy="afterInteractive"
+        src="//pl27373634.profitableratecpm.com/f74b3f2433134857ab82e88a59f9f2b2/invoke.js"
+        async
+        data-cfasync="false"
+      />
+      <div id="container-f74b3f2433134857ab82e88a59f9f2b2" />
+    </>
+  );
+}
